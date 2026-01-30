50eb617cda7dd771d837cfdce577ed538748d65f
edmondchancom <edmondchan.com@gmail.com>
2026-01-30 16:28:34 +0800

    desserts

--- src/App.js
+++ src/App.js
@@ -9,34 +9,57 @@ function App() {
     const [black, setBlack] = useState(0);
     const [white, setWhite] = useState(0);
 
-    // 小食清單
-    const snackItems = [
-        { key: "fries", name: "黃金脆薯", price: 13 },
-        { key: "pumpkinTempura", name: "南瓜天婦羅", price: 13 },
-        { key: "shrimpTempuraSet", name: "炸蝦天婦羅拼盤(半份)", price: 18 },
-        { key: "shrimpTempura", name: "炸蝦天婦羅", price: 18 },
-        { key: "chickenCartilage", name: "炸雞軟骨", price: 19 },
-        { key: "steamedEgg", name: "茶碗蒸", price: 19 },
-        { key: "friedChicken", name: "脆脆炸雞塊", price: 22 },
+    // 副餐類清單
+    const sideItems = [
+        { key: "pepperSauce", name: "青唐辛子燒椒醬", price: 3, image: "0.png" },
+        { key: "fries", name: "黃金脆薯", price: 13, image: "1.png" },
+        { key: "pumpkinTempura", name: "南瓜天婦羅", price: 13, image: "2.png" },
+        { key: "shrimpTempuraSetHalf", name: "炸蝦天婦羅拼盤(半份)", price: 18, image: "3.png" },
+        { key: "shrimpTempura", name: "炸蝦天婦羅", price: 18, image: "4.png" },
+        { key: "chickenCartilage", name: "炸雞軟骨", price: 19, image: "5.png" },
+        { key: "steamedEgg", name: "茶碗蒸", price: 19, image: "6.png" },
+        { key: "friedChicken", name: "脆脆炸雞塊", price: 22, image: "7.png" },
+        { key: "shrimpTempuraSetFull", name: "炸蝦天婦羅拼盤", price: 28, image: "8.png" },
+        { key: "salmonSashimiSet", name: "生三文魚刺身盛", price: 39, image: "9.png" },
+        { key: "redShrimpSashimi", name: "赤蝦刺身", price: 39, image: "10.png" },
+        { key: "premiumSashimiSet", name: "極上刺身盛合", price: 75, image: "11.png" },
+        { key: "salmonBellySashimi", name: "生三文魚腩刺身", price: 48, image: "12.png" },
+        { key: "salmonAmberjackBelly", name: "生三文魚腩刺身・寒鰤魚腩刺身", price: 48, image: "13.png" },
     ];
 
-    // 甜品清單
+    // 甜品・飲料清單
     const dessertItems = [
-        { key: "melonSorbet", name: "蜜瓜味雪葩", price: 13 },
-        { key: "frozenMango", name: "冷凍芒果", price: 13 },
-        { key: "milkIceCookie", name: "牛奶味軟雪糕(有曲奇)", price: 18 },
-        { key: "milkIce", name: "牛奶味軟雪糕", price: 18 },
-        { key: "pudding", name: "壽司郎經典布甸", price: 22 },
-        { key: "catalana", name: "卡達拉娜", price: 22 },
-        { key: "milleCrepe", name: "千層蛋糕", price: 22 },
-        { key: "peachMontBlanc", name: "白桃蒙布朗蛋糕", price: 22 },
-        { key: "matchaParfait", name: "抹茶蕨餅芭菲", price: 27 },
-        { key: "chocoBerryParfait", name: "朱古力香莓芭菲", price: 27 },
+        { key: "melonSorbet", name: "蜜瓜味雪葩", price: 13, image: "A1.png" },
+        { key: "frozenMango", name: "冷凍芒果", price: 13, image: "A2.png" },
+        { key: "milkSoftCookie", name: "牛奶味軟雪糕(有曲奇)", price: 18, image: "A3.png" },
+        { key: "milkSoft", name: "牛奶味軟雪糕", price: 18, image: "A4.png" },
+        { key: "classicPudding", name: "壽司郎經典布甸", price: 22, image: "A5.png" },
+        { key: "catalana", name: "卡達拉娜", price: 22, image: "A6.png" },
+        { key: "milleCrepe", name: "千層蛋糕", price: 22, image: "A7.png" },
+        { key: "peachMontBlanc", name: "白桃蒙布朗蛋糕", price: 22, image: "A8.png" },
+        { key: "matchaParfait", name: "抹茶蕨餅芭菲", price: 27, image: "A9.png" },
+        { key: "chocoBerryParfait", name: "朱古力香莓芭菲", price: 27, image: "A10.png" },
+        { key: "pepsiZero", name: "百事可樂(無糖)", price: 8, image: "A11.png" },
+        { key: "pepsi", name: "百事可樂", price: 8, image: "A12.jpg" },
+        { key: "mirinda", name: "美年達橙汁", price: 8, image: "A13.jpg" },
+        { key: "sevenUp", name: "七喜", price: 8, image: "A14.png" },
+        { key: "vitaLemonTea", name: "維他氣泡檸檬茶", price: 8, image: "A15.png" },
+        { key: "appleJuice", name: "100％蘋果汁", price: 8, image: "A16.png" },
+        { key: "slimTea", name: "纖解茶", price: 9, image: "A17.png" },
+        { key: "kikuMasamune", name: "菊正宗", price: 17, image: "A18.png" },
+        { key: "horoyoiGrape", name: "Horoyoi(葡萄味)", price: 19, image: "A19.png" },
+        { key: "horoyoiYuzu", name: "Horoyoi(柚子鹽味)", price: 19, image: "A20.jpg" },
+        { key: "suntoryBeer", name: "三得利頂級啤酒", price: 24, image: "A21.png" },
+        { key: "umeshu", name: "梅酒(含梅子)", price: 27, image: "A22.png" },
+        { key: "whiskySoda", name: "威士忌梳打", price: 27, image: "A23.png" },
+        { key: "kikuMasamuneDaiginjo", name: "菊正宗(大吟釀)", price: 59, image: "A24.png" },
     ];
+
+
 
     // 狀態：為每個品項建立一個 useState
     const [counts, setCounts] = useState(
-        [...snackItems, ...dessertItems].reduce((acc, item) => {
+        [...sideItems, ...dessertItems].reduce((acc, item) => {
             acc[item.key] = 0;
             return acc;
         }, {})
@@ -57,6 +80,22 @@ function App() {
         setOpenSection(openSection === section ? null : section);
     };
 
+    // 其他項目清單，每個項目有 price 和 qty
+    const [others, setOthers] = useState([{ price: 0, qty: 0 }]);
+    const addOther = () => {
+        setOthers([...others, { price: 0, qty: 0 }]);
+    };
+
+    const updateOther = (index, field, value) => {
+        const newOthers = [...others];
+        newOthers[index][field] = Number(value);
+        setOthers(newOthers);
+    };
+
+
+
+
+
     // 總計
     const netTotal =
         red * prices.red +
@@ -64,10 +103,14 @@ function App() {
         silver * prices.silver +
         black * prices.black +
         white * prices.white +
-        [...snackItems, ...dessertItems].reduce(
+        [...sideItems, ...dessertItems].reduce(
             (sum, item) => sum + counts[item.key] * item.price,
             0
-        );
+        ) +
+        others.reduce((sum, item) => sum + item.price * item.qty, 0);
+
+
+
 
     const serviceFee = (netTotal * 0.1).toFixed(2);
     const grossTotal = (netTotal + parseFloat(serviceFee)).toFixed(2);
@@ -77,10 +120,13 @@ function App() {
         silver +
         black +
         white +
-        [...snackItems, ...dessertItems].reduce(
+        [...sideItems, ...dessertItems].reduce(
             (sum, item) => sum + counts[item.key],
             0
-        );
+        ) +
+        others.reduce((sum, item) => sum + item.qty, 0);
+
+
 
     const resetAll = () => {
         setRed(0);
@@ -89,7 +135,7 @@ function App() {
         setBlack(0);
         setWhite(0);
         setCounts(
-            [...snackItems, ...dessertItems].reduce((acc, item) => {
+            [...sideItems, ...dessertItems].reduce((acc, item) => {
                 acc[item.key] = 0;
                 return acc;
             }, {})
@@ -106,8 +152,12 @@ function App() {
                 minHeight: "100vh",
             }}
         >
-            <img src="/logo.png" alt="Logo" className="logo" />
-            <h1 style={{ marginLeft: "70px" }}>壽司郎碟計算器</h1>
+            <div className="header">
+                <img src="/logo.png" alt="Logo" className="logo" />
+                <h1>壽司郎帳單計算器</h1>
+            </div>
+
+
 
             {/* 壽司碟 */}
             <div className="plate-row">
@@ -145,14 +195,44 @@ function App() {
                 <button onClick={() => setWhite(white > 0 ? white - 1 : 0)}>-1</button>
             </div>
 
-            {/* Accordion - 小食 */}
-            <h2 onClick={() => toggleSection("snacks")}>
-                {openSection === "snacks" ? "▼" : "▶"} 小食
+            {/* Accordion - 副餐類  */}
+            <h2 onClick={() => toggleSection("sides")}>
+                {openSection === "sides" ? "▼" : "▶"} 副餐類
             </h2>
-            {openSection === "snacks" && (
+            {openSection === "sides" && (
                 <div>
-                    {snackItems.map((item) => (
+                    {sideItems.map((item) => (
                         <div key={item.key} className="plate-row">
+                            <img
+                                src={`${process.env.PUBLIC_URL}/${item.image}`}
+                                alt={item.name}
+                                className="thumb-icon"
+                            />
+                            <p>
+                                {item.name}: {counts[item.key]} (HK$ {counts[item.key] * item.price})
+                            </p>
+                            <button onClick={() => setCounts({ ...counts, [item.key]: counts[item.key] + 1 })}>+1</button>
+                            <button onClick={() => setCounts({ ...counts, [item.key]: counts[item.key] > 0 ? counts[item.key] - 1 : 0 })}>-1</button>
+                        </div>
+                    ))}
+
+
+                </div>
+            )}
+
+            {/* Accordion - 甜品・飲料 */}
+            <h2 onClick={() => toggleSection("desserts")}>
+                {openSection === "desserts" ? "▼" : "▶"} 甜品・飲料
+            </h2>
+            {openSection === "desserts" && (
+                <div>
+                    {dessertItems.map((item) => (
+                        <div key={item.key} className="plate-row">
+                            <img
+                                src={`${process.env.PUBLIC_URL}/${item.image}`}
+                                alt={item.name}
+                                className="thumb-icon"
+                            />
                             <p>
                                 {item.name}: {counts[item.key]} (HK$ {counts[item.key] * item.price})
                             </p>
@@ -178,38 +258,45 @@ function App() {
                 </div>
             )}
 
-            {/* Accordion - 甜品 */}
-            <h2 onClick={() => toggleSection("desserts")}>
-                {openSection === "desserts" ? "▼" : "▶"} 甜品
+
+
+            {/* Accordion - 其他 */}
+            <h2 onClick={() => toggleSection("others")}>
+                {openSection === "others" ? "▼" : "▶"} 其他
             </h2>
-            {openSection === "desserts" && (
+            {openSection === "others" && (
                 <div>
-                    {dessertItems.map((item) => (
-                        <div key={item.key} className="plate-row">
+                    {others.map((item, index) => (
+                        <div key={index} className="plate-row">
+                            <label>
+                                單價：
+                                <input
+                                    type="number"
+                                    value={item.price}
+                                    onChange={(e) => updateOther(index, "price", e.target.value)}
+                                />
+                            </label>
+                            <label>
+                                數量：
+                                <input
+                                    type="number"
+                                    value={item.qty}
+                                    onChange={(e) => updateOther(index, "qty", e.target.value)}
+                                />
+                            </label>
                             <p>
-                                {item.name}: {counts[item.key]} (HK$ {counts[item.key] * item.price})
+                                總計：<span style={{ color: "red" }}>HK$ {item.price * item.qty}</span>
                             </p>
-                            <button
-                                onClick={() =>
-                                    setCounts({ ...counts, [item.key]: counts[item.key] + 1 })
-                                }
-                            >
-                                +1
-                            </button>
-                            <button
-                                onClick={() =>
-                                    setCounts({
-                                        ...counts,
-                                        [item.key]: counts[item.key] > 0 ? counts[item.key] - 1 : 0,
-                                    })
-                                }
-                            >
-                                -1
-                            </button>
                         </div>
                     ))}
+                    <button onClick={addOther}>新增其他項目</button>
                 </div>
             )}
+
+
+
+
+
 
             <hr />
             <h2>總項目數: {totalItems}</h2>
