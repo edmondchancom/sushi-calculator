50986f2db9f019d9a9a52f18ea5944ef5784d1f6
edmondchancom <edmondchan.com@gmail.com>
2026-01-30 14:38:03 +0800

    plate logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 function App() {
     // 壽司碟
     const [red, setRed] = useState(0);
-    const [blue, setBlue] = useState(0);   // 藍碟 (你原本的)
     const [gold, setGold] = useState(0);
     const [silver, setSilver] = useState(0);
     const [black, setBlack] = useState(0);
@@ -18,12 +17,11 @@ function App() {
 
     // 價格表
     const prices = {
-        red: 12,       // 基本款式
-        blue: 15,      // 藍碟 (中價)
-        gold: 20,      // 金碟 (高價)
-        silver: 17,    // 銀碟 (中價)
-        black: 27,     // 黑碟 (特選/奢華)
-        white: 10,     // 白碟 (最低價)
+        red: 12,
+        silver: 17,
+        gold: 22,
+        black: 27,
+        white: 10,
         fries: 13,
         steamedEgg: 19,
         friedChicken: 22,
@@ -39,7 +37,6 @@ function App() {
     // 總計
     const netTotal =
         red * prices.red +
-        blue * prices.blue +
         gold * prices.gold +
         silver * prices.silver +
         black * prices.black +
@@ -53,7 +50,6 @@ function App() {
     const grossTotal = (netTotal + parseFloat(serviceFee)).toFixed(2);
     const totalItems =
         red +
-        blue +
         gold +
         silver +
         black +
@@ -65,7 +61,6 @@ function App() {
 
     const resetAll = () => {
         setRed(0);
-        setBlue(0);
         setGold(0);
         setSilver(0);
         setBlack(0);
@@ -90,40 +85,39 @@ function App() {
             <h1 style={{ marginLeft: "70px" }}>壽司郎碟計算器</h1>
 
             {/* 壽司碟 */}
-            <div>
+            <div className="plate-row">
+                <img src={`${process.env.PUBLIC_URL}/Red.JPEG`} alt="紅碟" className="plate-icon" />
                 <p>紅碟: {red} (HK$ {red * prices.red})</p>
-                <button onClick={() => setRed(red + 1)}>加一碟 (+HK$ {prices.red})</button>
-                <button onClick={() => setRed(red > 0 ? red - 1 : 0)}>減一碟</button>
+                <button onClick={() => setRed(red + 1)}>+1</button>
+                <button onClick={() => setRed(red > 0 ? red - 1 : 0)}>-1</button>
             </div>
 
-            <div>
-                <p>藍碟: {blue} (HK$ {blue * prices.blue})</p>
-                <button onClick={() => setBlue(blue + 1)}>加一碟 (+HK$ {prices.blue})</button>
-                <button onClick={() => setBlue(blue > 0 ? blue - 1 : 0)}>減一碟</button>
-            </div>
-
-            <div>
-                <p>金碟: {gold} (HK$ {gold * prices.gold})</p>
-                <button onClick={() => setGold(gold + 1)}>加一碟 (+HK$ {prices.gold})</button>
-                <button onClick={() => setGold(gold > 0 ? gold - 1 : 0)}>減一碟</button>
-            </div>
-
-            <div>
+            <div className="plate-row">
+                <img src={`${process.env.PUBLIC_URL}/Silver.JPEG`} alt="銀碟" className="plate-icon" />
                 <p>銀碟: {silver} (HK$ {silver * prices.silver})</p>
-                <button onClick={() => setSilver(silver + 1)}>加一碟 (+HK$ {prices.silver})</button>
-                <button onClick={() => setSilver(silver > 0 ? silver - 1 : 0)}>減一碟</button>
+                <button onClick={() => setSilver(silver + 1)}>+1</button>
+                <button onClick={() => setSilver(silver > 0 ? silver - 1 : 0)}>-1</button>
             </div>
 
-            <div>
+            <div className="plate-row">
+                <img src={`${process.env.PUBLIC_URL}/Gold.JPEG`} alt="金碟" className="plate-icon" />
+                <p>金碟: {gold} (HK$ {gold * prices.gold})</p>
+                <button onClick={() => setGold(gold + 1)}>+1</button>
+                <button onClick={() => setGold(gold > 0 ? gold - 1 : 0)}>-1</button>
+            </div>
+
+            <div className="plate-row">
+                <img src={`${process.env.PUBLIC_URL}/Black.JPEG`} alt="黑碟" className="plate-icon" />
                 <p>黑碟: {black} (HK$ {black * prices.black})</p>
-                <button onClick={() => setBlack(black + 1)}>加一碟 (+HK$ {prices.black})</button>
-                <button onClick={() => setBlack(black > 0 ? black - 1 : 0)}>減一碟</button>
+                <button onClick={() => setBlack(black + 1)}>+1</button>
+                <button onClick={() => setBlack(black > 0 ? black - 1 : 0)}>-1</button>
             </div>
 
-            <div>
+            <div className="plate-row">
+                <img src={`${process.env.PUBLIC_URL}/White.JPEG`} alt="白碟" className="plate-icon" />
                 <p>白碟: {white} (HK$ {white * prices.white})</p>
-                <button onClick={() => setWhite(white + 1)}>加一碟 (+HK$ {prices.white})</button>
-                <button onClick={() => setWhite(white > 0 ? white - 1 : 0)}>減一碟</button>
+                <button onClick={() => setWhite(white + 1)}>+1</button>
+                <button onClick={() => setWhite(white > 0 ? white - 1 : 0)}>-1</button>
             </div>
 
             {/* Accordion - 小食 */}
