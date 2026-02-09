7c338bfbabdbe16d9b8257ef2c07f90f99de3bc3
edmondchancom <edmondchan.com@gmail.com>
2026-02-09 11:34:51 +0800

    w5

--- src/App.js
+++ src/App.js
@@ -213,17 +213,22 @@ function App() {
                 />
 
                 <div style={{ display: "flex", flexDirection: "column" }}>
-                    {/* 第一行：數量 × 單價 = 總額 */}
+                    {/* 第一行：數量 × 單價 */}
                     <div style={{ display: "flex", alignItems: "center" }}>
-                        <span>白碟: {white} × HK${whitePrice} = HK${white * whitePrice}</span>
+                        <span style={{ width: "160px" }}>
+                            白碟: {white} × HK${whitePrice}
+                        </span>
+                        <button onClick={() => setWhitePrice(whitePrice + 1)} style={{ marginLeft: "10px" }}>$+1</button>
+                        <button onClick={() => setWhitePrice(whitePrice > 0 ? whitePrice - 1 : 0)} style={{ marginLeft: "5px" }}>$-1</button>
                     </div>
 
-                    {/* 第二行：所有按鈕 */}
+                    {/* 第二行：總額 */}
                     <div style={{ display: "flex", alignItems: "center", marginTop: "5px" }}>
-                        <button onClick={() => setWhitePrice(whitePrice + 1)} style={{ marginRight: "5px" }}>$+1</button>
-                        <button onClick={() => setWhitePrice(whitePrice > 0 ? whitePrice - 1 : 0)} style={{ marginRight: "15px" }}>$-1</button>
-                        <button onClick={() => setWhite(white + 1)} style={{ marginRight: "5px" }}>+1</button>
-                        <button onClick={() => setWhite(white > 0 ? white - 1 : 0)}>-1</button>
+                        <span style={{ width: "160px" }}>
+                            = HK${white * whitePrice}
+                        </span>
+                        <button onClick={() => setWhite(white + 1)} style={{ marginLeft: "10px" }}>+1</button>
+                        <button onClick={() => setWhite(white > 0 ? white - 1 : 0)} style={{ marginLeft: "5px" }}>-1</button>
                     </div>
                 </div>
             </div>
